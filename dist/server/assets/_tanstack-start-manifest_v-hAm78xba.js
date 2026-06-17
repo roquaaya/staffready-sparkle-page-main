67128ec7eb36f7b4e2548df12ac644d3fbaf19e7
roquaaya <roquaayawalead901@gmail.com>
2026-06-17 22:06:23 +0300

    enable dist  folder

---
+++ dist/server/assets/_tanstack-start-manifest_v-hAm78xba.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Genius/Desktop/شغل شركه نقل وسياحه/staffready-sparkle-page-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-DzEbfoTD.js"] }, "/": { filePath: "C:/Users/Genius/Desktop/شغل شركه نقل وسياحه/staffready-sparkle-page-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-CLmh65Tx.js"] } }, clientEntry: "/assets/index-DzEbfoTD.js" });
+export {
+  tsrStartManifest
+};
---
+++ dist/server/index.js
@@ -0,0 +1,9 @@
+import { _, $ } from "./assets/worker-entry-3LOlq5tT.js";
+import "node:events";
+import "node:async_hooks";
+import "node:stream/web";
+import "node:stream";
+export {
+  _ as createServerEntry,
+  $ as default
+};
