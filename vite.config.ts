00110cfcbf7940d01856a47006f68dd6c7f79453
roquaaya <roquaayawalead901@gmail.com>
2026-06-18 11:41:04 +0300

    fix: change base path to root

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./staff-ready-landing/", 
+  base: "./", 
 })
