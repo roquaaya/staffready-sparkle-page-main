f05fa533b76409927f5b7df0759b34f1388c98d4
roquaaya <roquaayawalead901@gmail.com>
2026-06-18 14:08:40 +0300

    update project files

--- dist/server/index.js
+++ dist/server/index.js
@@ -1,4 +1,4 @@
-import { _, $ } from "./assets/worker-entry-CcEaOhAe.js";
+import { _, $ } from "./assets/worker-entry-DI7MDA63.js";
 import "node:events";
 import "node:async_hooks";
 import "node:stream/web";
