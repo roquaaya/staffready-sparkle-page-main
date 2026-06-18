598afa737b668310cfd3bd5658a05859ec49aad1
roquaaya <roquaayawalead901@gmail.com>
2026-06-18 13:43:58 +0300

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-B0iP5QJ2.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Genius/Desktop/شغل شركه نقل وسياحه/staffready-sparkle-page-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/./staff-ready-landing/assets/index-ndOYe5E5.js"] }, "/": { filePath: "C:/Users/Genius/Desktop/شغل شركه نقل وسياحه/staffready-sparkle-page-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/./staff-ready-landing/assets/index-DmpsbWmy.js"] } }, clientEntry: "/./staff-ready-landing/assets/index-ndOYe5E5.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/assets/_tanstack-start-manifest_v-D9l3si15.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Genius/Desktop/شغل شركه نقل وسياحه/staffready-sparkle-page-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/staff-ready/assets/index-DaGgRNS-.js"] }, "/": { filePath: "C:/Users/Genius/Desktop/شغل شركه نقل وسياحه/staffready-sparkle-page-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/staff-ready/assets/index-CVpeUGx4.js"] } }, clientEntry: "/staff-ready/assets/index-DaGgRNS-.js" });
+export {
+  tsrStartManifest
+};
